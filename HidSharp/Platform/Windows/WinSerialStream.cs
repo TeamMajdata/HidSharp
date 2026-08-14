ed32bebc274c9b547fd60045ab372283adb64c94
LeZi9916 <lezi@leziblog.com>
2026-08-14 10:57:06 +0800

    fix(win): sb unity

--- HidSharp/Platform/Windows/WinSerialStream.cs
+++ HidSharp/Platform/Windows/WinSerialStream.cs
@@ -19,7 +19,6 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 
 namespace HidSharp.Platform.Windows
 {
